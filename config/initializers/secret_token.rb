b847ea60b8b3d1d0be4b5090064b7283a9a8e889
Mateo Vidal <jvidalba@gmail.com>
2013-03-05 10:20:28 -0500

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialbike::Application.config.secret_token = '724a689a594cdd44ae52ae9ed317562c25ecaa5535f86e47b945f294a3867d59c9f4dec66a3c76992038334708a1d194bbc88b2cccd26c3828c6de6bacd9837b'
