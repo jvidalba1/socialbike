3b2645c4e499827ce92af560b95148e2ddcc89e7
Mateo Vidal <jvidalba@gmail.com>
2013-04-15 23:18:41 -0500

    mostrando eventos en perfil de usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @events = @user.events.paginate(:page => params[:page])
     @title = @user.name
   end
 
