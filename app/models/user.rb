13a1c0505e3033642d050273217fe69f5db5e925
Mateo Vidal <jvidalba@gmail.com>
2013-05-05 01:30:39 -0500

    followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,10 @@ class User < ActiveRecord::Base
   has_many :invitations
   has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy
   has_many :followed_users, :through => :relationships, :source => :followed
+  has_many :reverse_relationships,  :foreign_key => "followed_id",
+                                    :class_name  =>  "Relationship",
+                                    :dependent   =>   :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
